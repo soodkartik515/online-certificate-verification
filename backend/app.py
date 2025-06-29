8fb44a1585a837f75e2c0e3d730780dfc7e779cb
soodkartik515 <71901886+soodkartik515@users.noreply.github.com>
2025-06-29 13:27:59 +0530

    Serve frontend from static folder

--- backend/app.py
+++ backend/app.py
@@ -27,4 +27,3 @@ def verify_certificate():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
-Serve frontend from static folder
